84c9e41acce26d07bc87d3f6d18c0162ad21235d
codeagles <hcn0521@outlook.com>
2020-12-02 14:05:18 +0800

    初始化

---
+++ src/main/java/codeagles/Hello.java
@@ -0,0 +1,16 @@
+package codeagles;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Codeagles
+ * Date: 2020/12/2
+ * Time: 下午1:57
+ * <p>
+ * Description:
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
