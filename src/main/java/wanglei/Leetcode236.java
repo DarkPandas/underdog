c6ce230a3ff3b080be86a838504a23e12dcbcd08
wanglei <wanglei4@weipugroup.com>
2021-01-13 08:53:37 +0800

    236

---
+++ src/main/java/wanglei/Leetcode236.java
@@ -0,0 +1,41 @@
+package wanglei;
+
+import wanglei.struc.TreeNode;
+
+public class Leetcode236 {
+    /**
+     * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+     *
+     * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+     *
+     * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+     *
+     *
+     *
+     */
+
+    /**
+     * 思路：1. 运用递归，先去到最底层或者该节点等于给定的p或者q
+     *      2. 从下往上，左边比较是否存在公共节点，若存在，返回该值；同理右边比较是否存在；若都不存在，返回上一个节点重新此过程
+     *      代码中注释来源：leetcode.cn
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {//当遍历到叶结点后就会返回null
+            return root;
+        }
+        if (root == p || root == q) {//当找到p或者q的是时候就会返回pq
+            return root;/*当然，值得一提的是，如果公共祖先是自己（pq），并不需要寻找另外
+                     一个，我们在执行前序遍历会先找上面的，后找下面的，我们会直接返回公共祖先。*/
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);//返回的结点进行保存，可能是null
+        TreeNode right = lowestCommonAncestor(root.right, p, q);//也可能是pq，还可能是公共祖先
+        if (left != null && right != null) {
+            return root;//如果左右都存在，就说明pq都出现了，这就是，公共祖先，此时不用考虑公共祖先是自己的情况，因为上面已经做过判断了。
+        } else if (left != null) {//否则我们返回已经找到的那个值（存储在left，与right中），p或者q
+            return left;//还有一种可能就是，由下面返回的公共祖先，并将这个值一路返回到最表层
+        } else if (right != null) {
+            return right;
+        }
+        return null;
+    }
+}
